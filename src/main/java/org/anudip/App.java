7caddc7b0dfa65f28d60ed48cd88397a15f9f107
pooja m <poojamurthy413@gmail.com>
2024-10-23 17:06:01 +0530

    Added Solution.

--- src/main/java/org/anudip/App.java
+++ src/main/java/org/anudip/App.java
@@ -1,7 +1,39 @@
 package org.anudip;
 
+import org.anudip.javaapp02.operation.TwoNonDecimalNumbersDivision;
+
 public class App {
 	public static void main(String[] args) {
+		TwoNonDecimalNumbersDivision twoNonDecimalNumbersDivision = new TwoNonDecimalNumbersDivision();
 		
+		double result1 = twoNonDecimalNumbersDivision.divide(10,2);
+		System.out.println(result1);
+		
+		double result2 = twoNonDecimalNumbersDivision.divide(10,3);
+		System.out.println(result2);
+		
+		double result3 = twoNonDecimalNumbersDivision.divide(10,4);
+		System.out.println(result3);
+		
+		double result4 = twoNonDecimalNumbersDivision.divide(2,10);
+		System.out.println(result4);
+        		
+		double result5 = twoNonDecimalNumbersDivision.divide(10,-2);
+		System.out.println(result5);
+		
+		double result6 = twoNonDecimalNumbersDivision.divide(-10,2);
+		System.out.println(result6);
+		
+		double result7 = twoNonDecimalNumbersDivision.divide(-10,-2);
+		System.out.println(result7);
+		
+		double result8 = twoNonDecimalNumbersDivision.divide(10,0);
+		System.out.println(result8);
+		
+		double result9 = twoNonDecimalNumbersDivision.divide(0,2);
+		System.out.println(result9);
+		
+		double result10 = twoNonDecimalNumbersDivision.divide(0,0);
+		System.out.println(result10);
 	}
 }
