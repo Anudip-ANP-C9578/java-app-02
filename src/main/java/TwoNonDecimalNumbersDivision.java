af62490100eb166171d62d70126de57663d65f68
Khushi-86 <khushichhajer27@gmail.com>
2024-10-23 21:54:30 +0530

    Added solution for java-app-02.

---
+++ src/main/java/TwoNonDecimalNumbersDivision.java
@@ -0,0 +1,29 @@
+/*Program to perform division of two nondecimal numbers*/
+
+package org.anudip.javaapp02.operation;
+
+public class TwoNonDecimalNumbersDivision    /*This class performs the task division of two non-decimal numbers*/
+
+{
+	public double divide(int n1, int n2)  /*This method performs the task of division of two non-decimal numbers.*/
+
+       {
+	 double res= 0;
+		
+	   if((n1 < 0) || (n2 < 0)) 
+              {
+		 res = -1.0;   /*Return -1.0 if either number is negative*/
+	      } 
+           else if((n1 == 0) || (n2 ==0)) 
+              {
+		res = -2.0;  /*Return -2.0 if either number is negative*/
+             } 
+           else 
+             {
+                /*Explicit Type casting*/
+		res =(double)n1/n2; /*Performs division operation*/
+	     }
+		
+	  return res;  /*Returns result after division operation*/
+      }
+}
--- src/main/java/org/anudip/App.java
+++ src/main/java/org/anudip/App.java
@@ -1,7 +1,41 @@
 package org.anudip;
 
+import org.anudip.javaapp02.operation.TwoNonDecimalNumbersDivision ;
+
 public class App {
-	public static void main(String[] args) {
-		
-	}
+	public static void main(String[] args) 
+        {
+         TwoNonDecimalNumbersDivision twoNonDecimalNumbersDivision =new TwoNonDecimalNumbersDivision ();
+ 
+double res1=twoNonDecimalNumbersDivision.divide(10,2);
+System.out.println(res1);
+	
+double res2=twoNonDecimalNumbersDivision.divide(10,3);
+System.out.println(res2);
+
+double res3=twoNonDecimalNumbersDivision.divide(10,4);
+System.out.println(res3);
+
+double res4=twoNonDecimalNumbersDivision.divide(2,10);
+System.out.println(res4);
+
+double res5=twoNonDecimalNumbersDivision.divide(10,-2);
+System.out.println(res5);
+
+double res6=twoNonDecimalNumbersDivision.divide(-10,2);
+System.out.println(res6);
+
+double res7=twoNonDecimalNumbersDivision.divide(-10,-2);
+System.out.println(res7);
+
+double res8=twoNonDecimalNumbersDivision.divide(10,0);
+System.out.println(res8);
+
+double res9=twoNonDecimalNumbersDivision.divide(0,2);
+System.out.println(res9);
+
+double res10=twoNonDecimalNumbersDivision.divide(0,0);
+System.out.println(res10);
+
+      }
 }
